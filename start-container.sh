736edd14a007b6f6e19e40099be7728875e68f4f
Aidan Lardner <aj@ajlardner.net>
2026-07-03 23:46:52 -0400

    update workspace directory structure, update gitignore for new dir structure

--- start-container.sh
+++ start-container.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -it --env-file .env -v "$PWD:/workspace" -v .pi:/root/.pi pi-sandbox
+sudo docker run --rm -it --env-file .env -v "$PWD/workspace:/workspace" -v .pi:/root/.pi pi-sandbox
